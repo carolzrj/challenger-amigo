73370296d97ed04b2cc0842a5e0c0838cd53a6bc
carolzrj <carolinasgarcia97@gmail.com>
2025-01-28 17:08:45 -0300

    armazenando nomes

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = []
+
+function adicionarAmigo () {
+    let nomeAmigo = document.getElementById("amigo").value;
+    if (nomeAmigo == " ") {
+        alert ('Por favor, insira um nome!')
+    } else {
+        amigos.push(nomeAmigo);
+        document.getElementById("amigo").value = " ";
+    }
+}
