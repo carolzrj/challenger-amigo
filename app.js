d6e3bad10f6b0e0b77d786c413bd5f0f9c62fdd6
carolzrj <carolinasgarcia97@gmail.com>
2025-01-28 17:26:34 -0300

    adc e atualiza lista

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-let amigos = []
+let listaAmigos = []
 
 function adicionarAmigo () {
     let nomeAmigo = document.getElementById("amigo").value;
     if (nomeAmigo == " ") {
         alert ('Por favor, insira um nome!')
     } else {
-        amigos.push(nomeAmigo);
+        listaAmigos.push(nomeAmigo);
         document.getElementById("amigo").value = " ";
+        atualizarAmigos();
+    }
+}
+
+function atualizarAmigos () {
+    let amigosAtualizada = document.getElementById("listaAmigos");
+    amigosAtualizada.innerHTML = " ";
+    for (let i=0; i < amigos.length; i++) {
+        let itemAmigos = document.createElement("li");
+        itemAmigos.innerHTML = listaAmigos[i];
+        amigosAtualizada.appendChild(itemAmigos);
     }
 }
