59c440470bef197df6693f4a070ed59539eb4d5a
carolzrj <carolinasgarcia97@gmail.com>
2025-01-30 12:53:55 -0300

    inserindo alerta de repetição

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ let listaAmigos = []
 function adicionarAmigo () {
     let nomeAmigo = document.getElementById("amigo").value;
     if (nomeAmigo == " ") {
-        alert ('Por favor, insira um nome!')
-    } else {
+        alert ('Por favor, insira um nome!');
+    }
+    if (listaAmigos.includes(nomeAmigo)) {
+        alert ('Este nome já está incluso na lista.');
+    }
+    else {
         listaAmigos.push(nomeAmigo);
         document.getElementById("amigo").value = " ";
         atualizarAmigos();
@@ -14,9 +18,10 @@ function adicionarAmigo () {
 function atualizarAmigos () {
     let amigosAtualizada = document.getElementById("listaAmigos");
     amigosAtualizada.innerHTML = " ";
-    for (let i=0; i < amigos.length; i++) {
+    for (let i=0; i < listaAmigos.length; i++) {
         let itemAmigos = document.createElement("li");
         itemAmigos.innerHTML = listaAmigos[i];
         amigosAtualizada.appendChild(itemAmigos);
     }
 }
+
